38ec9d4f457d7dea57fc261bfcb41c345e8d16b7
Satyam Sharma <satyamsharma1725@gmail.com>
2023-08-18 22:02:49 +0530

    initial commit

---
+++ app/src/main/java/com/satverse/bookbox/others/Constants.kt
@@ -0,0 +1,12 @@
+package com.satverse.bookbox.others
+
+object Constants {
+    const val DOWNLOAD_DIR = "BookboxEbooks"
+    const val DATABASE_NAME = "bookbox.db"
+    const val TWITTER_URL = "https://twitter.com/satverse.tech"
+    const val REPO_URL = "https://github.com/satyamsharma17/Bookbox"
+    const val DEV_GITHUB_URL = "https://github.com/satyamsharma17"
+    const val DEV_TWITTER_URL = "https://twitter.com/iamsatyam17"
+    const val VIVEK_GITHUB_URL = "https://github.com/vivek-344"
+    const val VIVEK_TWITTER_URL = "https://twitter.com/vivekraj344"
+}
